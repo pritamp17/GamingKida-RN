624d6cc1babcea9260247803bed7c5dbeda1d4bc
Replit user <>
2023-09-27 05:51:38 +0000

    initial develop branch

--- services/chatService/messageFunctions.tsx
+++ services/chatService/messageFunctions.tsx
@@ -13,3 +13,16 @@ export const sendMessage = async (messageContent, senderId, chatId) => {
     console.error("Error sending message: ", error);
   }
 };
+
+export const fetchMessages = async (senderId, chatId) => {
+  try {
+    // Add a new message document to the specified 'chatId' collection
+    await db.collection('chats').doc(chatId).collection('messages').add({
+      content: messageContent,
+      senderId,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  } catch (error) {
+    console.error("Error sending message: ", error);
+  }
+};
