8b92f1e88d66f0d9de6ef8bfc19cea7d57662f77
Prïtåm Pawar <65030567+pritamp17@users.noreply.github.com>
2023-10-11 10:33:19 +0000

    orgService edit org remainign

--- services/orgService/crudOrg.js
+++ services/orgService/crudOrg.js
@@ -1,7 +1,115 @@
+import { db } from '../firebase/firebaseconfig';
 
-// import firebase from ../firebase/firebaseconfig
-const createOrg = (userId) => {
-
+class Org {
+    constructor(name, createdAt, adminIds, memberIds, competitionIds) {
+        this.name = name;
+        this.createdAt = createdAt;
+        this.adminIds = adminIds;
+        this.memberIds = memberIds;
+        this.competitionIds = competitionIds;
+    }
 }
 
-const getOrg = ()
+export const createOrg = async (org, userId) => {
+    // Validate org
+    if (!(org instanceof Org)) {
+        throw new Error('Expected argument of type "Org"');
+    }
+
+    // Reference to the Firestore collection
+    const orgCollection = db.collection('org');
+
+    // Check if the organization name is already taken
+    const nameCheck = await orgCollection.where('name', '==', org.name).get();
+    
+    if (!nameCheck.empty) {
+        console.error('Organization name is already taken!');
+        return false;
+    }
+
+    // Add userId to adminIds and memberIds
+    org.adminIds.push(userId);
+    org.memberIds.push(userId);
+
+    // Add the new org into Firestore and auto-generate an ID
+    return orgCollection.add({
+        name: org.name,
+        createdAt: org.createdAt,
+        adminIds: org.adminIds,
+        memberIds: org.memberIds,
+        competitionIds: org.competitionIds
+    })
+    .then((docRef) => {
+        console.log('Organization successfully created with ID: ', docRef.id);
+        return docRef.id; // Return the ID of the newly created doc
+    })
+    .catch((error) => {
+        console.error('Error creating organization: ', error);
+        return false;
+    });
+};
+
+export const searchOrg = async (name) => {
+    try {
+        // Reference to the Firestore collection
+        const orgCollection = db.collection('org');
+
+        // Search for the organization by name
+        const querySnapshot = await orgCollection.where('name', '==', name).get();
+
+        // Check if any documents are found
+        if (!querySnapshot.empty) {
+            // Get the first document (there should only be one due to name uniqueness)
+            const orgDoc = querySnapshot.docs[0];
+
+            // Return the organization data as an object
+            return {
+                id: orgDoc.id,
+                ...orgDoc.data()
+            };
+        } else {
+            console.error('No organization found with the provided name!');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error searching for organization: ', error);
+        return null;
+    }
+};
+
+export const deleteOrg = async (name, userId) => {
+    try {
+        // Reference to the Firestore collection
+        const orgCollection = db.collection('org');
+
+        // Search for the organization by name
+        const querySnapshot = await orgCollection.where('name', '==', name).get();
+
+        // Check if any documents are found
+        if (!querySnapshot.empty) {
+            // Get the first document (there should only be one due to name uniqueness)
+            const orgDoc = querySnapshot.docs[0];
+            
+            // Check if the userId is in the adminIds array
+            const orgData = orgDoc.data();
+            if (orgData.adminIds.includes(userId)) {
+                // Delete the organization
+                await orgCollection.doc(orgDoc.id).delete();
+                console.log(`Organization with name "${name}" has been deleted.`);
+                return true;
+            } else {
+                console.error("Error: User is not an admin of this organization and cannot delete it.");
+                return false;
+            }
+        } else {
+            console.error(`No organization found with the name "${name}"!`);
+            return false;
+        }
+    } catch (error) {
+        console.error('Error deleting organization: ', error);
+        return false;
+    }
+};
+
+
+export { Org };
--- services/userService/index.js
+++ services/userService/index.js
@@ -1,24 +1,41 @@
-import Firestore from '@react-native-firebase/firestore';
+export class User {
+  constructor(id, name, email, groupIds = [], competitionIds = [], connectionIds = []) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.groupIds = groupIds;
+      this.competitionIds = competitionIds;
+      this.connectionIds = connectionIds;
+  }
+}
 
 export const addUserToFirestore = async (user) => {
+  if (!(user instanceof User)) {
+      throw new Error('Expected argument of type "User"');
+  }
+
   try {
-    const userRef = Firestore().collection('users').doc(user.id);
-    const doc = await userRef.get();
-    
-    // Check if user exists, if not add them to Firestore
-    if (!doc.exists) {
-      await userRef.set({
-        name: user.name,
-        email: user.email,
-        groupId: [],
-        competitionId: [],
-        connectionId: [],
-        // ... other fields
-      });
-    }
-    return true;
+      const userRef = Firestore().collection('users').doc(user.id);
+      const doc = await userRef.get();
+
+      // Check if user exists, if not add them to Firestore
+      if (!doc.exists) {
+          await userRef.set({
+              name: user.name,
+              email: user.email,
+              groupIds: user.groupId,
+              competitionIds: user.competitionId,
+              connectionIds: user.connectionId,
+              // ... other fields
+          });
+          console.log("User added successfully!");
+          return true;
+      } else {
+          console.log("User already exists!");
+          return false;
+      }
   } catch (error) {
-    console.error("Error adding user: ", error);
-    return false;
+      console.error("Error adding user: ", error);
+      return false;
   }
 };
