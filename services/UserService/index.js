c903293531bb1c2c8beb2565221f67b2f492c43a
Prïtåm Pawar <65030567+pritamp17@users.noreply.github.com>
2023-10-18 07:23:07 +0000

    working

--- services/UserService/index.js
+++ services/UserService/index.js
@@ -1,41 +1,39 @@
+import { db } from '../Firebase/firebaseconfig';
+
 export class User {
-  constructor(id, name, email, groupIds = [], competitionIds = [], connectionIds = []) {
-      this.id = id;
-      this.name = name;
-      this.email = email;
-      this.groupIds = groupIds;
-      this.competitionIds = competitionIds;
-      this.connectionIds = connectionIds;
-  }
+    constructor(name, email, groupIds = [], competitionIds = [], connectionIds = []) {
+        this.name = name;
+        this.email = email;
+        this.groupIds = groupIds;
+        this.competitionIds = [];
+        this.connectionIds = [];
+    }
 }
 
 export const addUserToFirestore = async (user) => {
-  if (!(user instanceof User)) {
-      throw new Error('Expected argument of type "User"');
-  }
+    if (!(user instanceof User)) {
+        throw new Error('Expected argument of type "User"');
+    }
 
-  try {
-      const userRef = Firestore().collection('users').doc(user.id);
-      const doc = await userRef.get();
+    try {
+        // As Firebase generates the ID, we don't specify a .doc() argument
+        const userRef = db.collection('users').doc();
 
-      // Check if user exists, if not add them to Firestore
-      if (!doc.exists) {
-          await userRef.set({
-              name: user.name,
-              email: user.email,
-              groupIds: user.groupId,
-              competitionIds: user.competitionId,
-              connectionIds: user.connectionId,
-              // ... other fields
-          });
-          console.log("User added successfully!");
-          return true;
-      } else {
-          console.log("User already exists!");
-          return false;
-      }
-  } catch (error) {
-      console.error("Error adding user: ", error);
-      return false;
-  }
+        // Set the user data
+        await userRef.set({
+            name: user.name,
+            email: user.email,
+            groupIds: user.groupIds,
+            competitionIds: user.competitionIds,
+            connectionIds: user.connectionIds,
+            // ... other fields
+        });
+
+        console.log("User added successfully!");
+        return true;
+
+    } catch (error) {
+        console.error("Error adding user: ", error);
+        return false;
+    }
 };
