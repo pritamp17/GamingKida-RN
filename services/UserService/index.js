8d46e6bb823415b860dac924179833d8073d330c
Prïtåm Pawar <65030567+pritamp17@users.noreply.github.com>
2023-10-18 07:49:56 +0000

    userservice done

--- services/UserService/index.js
+++ services/UserService/index.js
@@ -1,39 +1,18 @@
-import { db } from '../Firebase/firebaseconfig';
+import { addUserToFirestore, getByUserId } from "./crudOpsUser";
 
 export class User {
-    constructor(name, email, groupIds = [], competitionIds = [], connectionIds = []) {
+    constructor(userId, name, email, groupIds = [], competitionIds = [], connectionIds = []) {
+        this.userId = userId;   /// we get it from google signIn
         this.name = name;
         this.email = email;
         this.groupIds = groupIds;
-        this.competitionIds = [];
-        this.connectionIds = [];
+        this.competitionIds = competitionIds;
+        this.connectionIds = connectionIds;
     }
 }
 
-export const addUserToFirestore = async (user) => {
-    if (!(user instanceof User)) {
-        throw new Error('Expected argument of type "User"');
-    }
-
-    try {
-        // As Firebase generates the ID, we don't specify a .doc() argument
-        const userRef = db.collection('users').doc();
-
-        // Set the user data
-        await userRef.set({
-            name: user.name,
-            email: user.email,
-            groupIds: user.groupIds,
-            competitionIds: user.competitionIds,
-            connectionIds: user.connectionIds,
-            // ... other fields
-        });
-
-        console.log("User added successfully!");
-        return true;
-
-    } catch (error) {
-        console.error("Error adding user: ", error);
-        return false;
-    }
-};
+export {
+    User,
+    addUserToFirestore,
+    getByUserId
+}
